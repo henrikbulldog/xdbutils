0a207dac73bc55a3b20f11f17f90741d45da91a0
Henrik Thomsen <xhtho@energinet.dk>
2023-03-21 12:37:39 +0000

    requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
 
 setuptools.setup(
-    install_requires=['pyspark', 'nest_asyncio']
+    install_requires=[
+        'pyspark',
+        'nest_asyncio',
+        'requests'
+        'boto3'
+        'azure-storage-file-datalake'
+        'azure-identity']
 )
