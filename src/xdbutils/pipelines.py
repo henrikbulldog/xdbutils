ae63217537a1cca055500e343e843bb8ed1beba9
Henrik Thomsen <henrik.thomsen.dk@gmail.com>
2023-07-13 14:03:05 +0000

    fix: pipeline.raw_to_bronze

--- src/xdbutils/pipelines.py
+++ src/xdbutils/pipelines.py
@@ -8,33 +8,28 @@ import dlt  # pylint: disable=import-error
 class Pipeline():
     """ Delta Live Tables Pipeline """
 
-    def raw_to_bronze(self,
+    def raw_to_bronze(
+        self,
         source_system,
         entity,
-        raw_base_path,
-        load: Callable[[str], DataFrame],
-        transform: Callable[[DataFrame], DataFrame] = None):
+        raw_df: DataFrame
+        ):
         """ Raw to bronze """
-
-        class_path = f"{source_system}/{entity}"
-        source_path = f"{raw_base_path}/{class_path}"
 
         @dlt.table(
             comment=f"Raw to Bronze, {source_system}.{entity}",
             name=f"bronze_{entity}"
         )
         def raw_to_bronze_table():
-            df = load(source_path)
-            if transform:
-                df = transform(df)
-            return df
+            return raw_df
 
     def bronze_to_silver(
         self,
         source_system,
         entity,
         transform: Callable[[DataFrame], DataFrame] = None,
-        expectations={}):
+        expectations={}
+        ):
         """ Bronze to Silver """
 
         @dlt.table(
@@ -62,7 +57,7 @@ class Pipeline():
             name=f"gold_{entity}_{name}"
         )
         @dlt.expect_all(expectations)
-        def silver_to_gold_table():
+        def silver_table():
             df = dlt.read(f"silver_{entity}")
             if transform:
                 df = transform(df)
